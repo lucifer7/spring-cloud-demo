e6e3d0d9eaf45f026f0657e05eabd4b081da8013
Jingyi.yang <jingyi.yang@derbysoft.com>
2017-01-05 18:04:51 +0800

    [Eureka] Add eureka server

--- src/main/java/com/yang/SpringCloudDemoApplication.java
+++ src/main/java/com/yang/SpringCloudDemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 /*@Configuration
 @EnableAutoConfiguration  *//* auto-configuration, designed to work well with Starters *//*
 @ComponentScan*/
+@EnableDiscoveryClient        /* difference ? */
+//@EnableEurekaClient
 public class SpringCloudDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudDemoApplication.class, args);
