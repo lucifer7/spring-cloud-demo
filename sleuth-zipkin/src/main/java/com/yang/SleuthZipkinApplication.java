e690d632e2041c7b4a28f94866ae9af73dc5ce38
lucifer <jingyi.yang@derbysoft.com>
2017-03-17 10:44:27 +0800

    [Sleuth + Zipkin] redirect log test

--- sleuth-zipkin/src/main/java/com/yang/SleuthZipkinApplication.java
+++ sleuth-zipkin/src/main/java/com/yang/SleuthZipkinApplication.java
@@ -1,6 +1,8 @@
 package com.yang;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -9,12 +11,17 @@ import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import zipkin.Span;
 
 @SpringBootApplication
-@RestController
 @Slf4j
 public class SleuthZipkinApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     @ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
@@ -33,13 +40,30 @@ public class SleuthZipkinApplication {
         return new AlwaysSampler();
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthZipkinApplication.class, args);
+    }
+}
+
+@RestController
+@Slf4j
+class WebController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${app.url:http://localhost:${local.server.port:${server.port:8080}}}")
+    private String url;
+
     @RequestMapping("/")
     public String home() {
         log.info("Visit home page.");
         return "Hello, sleuth with zipkin.";
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(SleuthZipkinApplication.class, args);
+    @RequestMapping("/callhome")
+    public String callHome() {
+        log.info("calling home");
+        return restTemplate.getForObject(url, String.class);
     }
+
 }
